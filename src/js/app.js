977c8e749b178646bd72b0dae9f41be57fa7084d
Eldar Giniyatov <eldar.giniyatov@sitesoft.ru>
2018-03-19 16:08:15 +0500

    add swipe menu

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,27 @@
   'use strict';
 
   $(function () {
-      console.log('test JS')
+
+    // Init swipe-menu
+    $(window).load(function(){
+      $("[data-toggle]").click(function() {
+        var toggle_el = $(this).data("toggle");
+        $(toggle_el).toggleClass("open-sidebar");
+      });
+      $(".swipe-area").swipe({
+        swipeStatus:function(event, phase, direction, distance, duration, fingers)
+        {
+          if (phase=="move" && direction =="right") {
+            $(".container").addClass("open-sidebar");
+            return false;
+          }
+          if (phase=="move" && direction =="left") {
+            $(".container").removeClass("open-sidebar");
+            return false;
+          }
+        }
+      });
+    });
   });
 
 })(jQuery, window, document);
